f191e5ff245ef9429ccd57f37e097a91ed3150cf
黄煜恒 <hyhlryf@gmail.com>
2017-07-25 15:38:06 +0800

    First version of recognition

--- test.m
+++ test.m
@@ -3,13 +3,13 @@ I = imread(path);
 % I = rgb2gray(I);
 %% Image normalizer
 
-load('norCUFS.mat');
-index = randi([1,100]);
-
-subplot(1,2,1)
-imshow(T(:,:,index * 2));
-subplot(1,2,2)
-imshow(T(:,:,index * 2 + 1));
+% load('norCUFS.mat');
+% index = randi([1,100]);
+% 
+% subplot(1,2,1)
+% imshow(T(:,:,index * 2));
+% subplot(1,2,2)
+% imshow(T(:,:,index * 2 + 1));
 
 
 
@@ -42,3 +42,18 @@ imshow(T(:,:,index * 2 + 1));
 %     [d,~] = sift(patches(:,:,i));
 %     count = count + size(d,1);
 % end
+
+%% Test LDA
+% Generate example data: 2 groups, of 10 and 15, respectively
+X = [randn(10,2); randn(15,2) + 1.5];  Y = [zeros(10,1); ones(15,1)];
+
+% Calculate linear discriminant coefficients
+W = LDA(X,Y);
+W2 = W(:,2:end);
+
+% Calulcate linear scores for training data
+L = [ones(25,1) X] * W';
+L2 = X * W2';
+% Calculate class probabilities
+P = exp(L) ./ repmat(sum(exp(L),2),[1 2]);
+P2 = exp(L2) ./ repmat(sum(exp(L2),2),[1 2]);
