2641ddc87cff462b639641818e64959336be86a0
黄煜恒 <hyhlryf@gmail.com>
2017-07-26 20:10:56 +0800

    Fix bugs in training step.

--- train.m
+++ train.m
@@ -27,56 +27,73 @@ clear;
 
 %% Extract features
 
-load('norCUFS.mat');
-
-trainRate = 0.7;
-dataSize = size(T, 3) * trainRate;
-
-dataset = T(:,:,1:dataSize);
-nt = size(dataset,3)/2;
-
-Tmc = zeros(143*236,nt*2);
-Tsc = zeros(143*128,nt*2);
-Tmd = zeros(143*236,nt*2);
-Tsd = zeros(143*128,nt*2);
-Tmg = zeros(143*236,nt*2);
-Tsg = zeros(143*128,nt*2);
-
-for i = 1 : 2 * nt
-    Tmc(:,i,1) = featureExtraction(dataset(:,:,i), 'MLBP', 'csdn');
-    Tsc(:,i,2) = featureExtraction(dataset(:,:,i), 'SIFT', 'csdn');
-    Tmd(:,i,3) = featureExtraction(dataset(:,:,i), 'MLBP', 'dog');
-    Tsd(:,i,4) = featureExtraction(dataset(:,:,i), 'SIFT', 'dog');
-    Tmg(:,i,5) = featureExtraction(dataset(:,:,i), 'MLBP', 'gaussian');
-    Tsg(:,i,6) = featureExtraction(dataset(:,:,i), 'SIFT', 'gaussian');
-end
-save('featureVectorsfeatureVectors.mat',...
-    'Tmc','Tsc',...
-    'Tmd','Tsd',...
-    'Tmg','Tsg');
+% load('norCUFS.mat');
+% 
+% trainRate = 0.7;
+% dataSize = size(T, 3) * trainRate;
+% 
+% dataset = T(:,:,1:dataSize);
+% nt = size(dataset,3)/2;
+% 
+% Tmc = zeros(143*236,nt*2);
+% Tsc = zeros(143*128,nt*2);
+% Tmd = zeros(143*236,nt*2);
+% Tsd = zeros(143*128,nt*2);
+% Tmg = zeros(143*236,nt*2);
+% Tsg = zeros(143*128,nt*2);
+% 
+% for i = 1 : 2 * nt
+%     Tmc(:,i) = featureExtraction(dataset(:,:,i), 'MLBP', 'csdn');
+%     Tsc(:,i) = featureExtraction(dataset(:,:,i), 'SIFT', 'csdn');
+%     Tmd(:,i) = featureExtraction(dataset(:,:,i), 'MLBP', 'dog');
+%     Tsd(:,i) = featureExtraction(dataset(:,:,i), 'SIFT', 'dog');
+%     Tmg(:,i) = featureExtraction(dataset(:,:,i), 'MLBP', 'gaussian');
+%     Tsg(:,i) = featureExtraction(dataset(:,:,i), 'SIFT', 'gaussian');
+% end
+% save('featureVectors.mat',...
+%     'Tmc','Tsc',...
+%     'Tmd','Tsd',...
+%     'Tmg','Tsg');
 
 %% Represent prototype
-
+clear;
 load('featureVectors.mat');
 l = size(Tmc, 2);
 nt = int32(l/2);
 X = NaN(nt, 2*nt,6);
 
 X(:,:,1) = prototypeRepresentation(Tmc(:,:));
-X(:,:,1) = prototypeRepresentation(Tsc(:,:));
-X(:,:,1) = prototypeRepresentation(Tmd(:,:));
-X(:,:,1) = prototypeRepresentation(Tsd(:,:));
-X(:,:,1) = prototypeRepresentation(Tmg(:,:));
-X(:,:,1) = prototypeRepresentation(Tsg(:,:));
+X(:,:,2) = prototypeRepresentation(Tsc(:,:));
+X(:,:,3) = prototypeRepresentation(Tmd(:,:));
+X(:,:,4) = prototypeRepresentation(Tsd(:,:));
+X(:,:,5) = prototypeRepresentation(Tmg(:,:));
+X(:,:,6) = prototypeRepresentation(Tsg(:,:));
 save('prototype.mat','X');
 %% Discriminant analysis
 W = NaN(nt, nt+1, 6);
-mu = NaN(6, 1);
+mu = NaN(nt, 6);
 for i = 1 : 6
-    [~, score, ~, ~, ~, mu(i,:)] = pca(transpose(X(:,:,i)));
+    [~, score, ~, ~, ~, mu(:, i)] = pca(transpose(X(:,:,i)));
 %     meancenterX = bsxfun(@minus, X, mean(X)); %uncertain
     Y = repmat(1:1:nt/2,2,1);
     Y = Y(:);
-    W(:,:,i) = LDA(transpose(X(:,:,i)), Y);
+    switch i
+        case 1
+            Wmc = LDA(transpose(X(:,:,i)), Y);
+        case 2
+            Wsc = LDA(transpose(X(:,:,i)), Y);
+        case 3
+            Wmd = LDA(transpose(X(:,:,i)), Y);
+        case 4
+            Wsd = LDA(transpose(X(:,:,i)), Y);
+        case 5
+            Wmg = LDA(transpose(X(:,:,i)), Y);
+        case 6
+            Wsg = LDA(transpose(X(:,:,i)), Y);
+        
+    end
 end
-save('prototype.mat','W','mu','dataset');
+save('prototype.mat','mu',...
+    'Wmc','Wsc',...
+    'Wsg','Wmg',...
+    'Wsd','Wmd');
